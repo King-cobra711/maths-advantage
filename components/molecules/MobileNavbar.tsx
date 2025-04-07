c74e01f2fd54416616a21eb17885afc967e3d9fe
Matthew Hansen <72847525+King-cobra711@users.noreply.github.com>
2025-04-07 14:19:31 +1000

    remove hamburger transition for now

--- components/molecules/MobileNavbar.tsx
+++ components/molecules/MobileNavbar.tsx
@@ -1,33 +1,20 @@
 "use client";
-
 import { Enriqueta } from "next/font/google";
-import { useState, useEffect } from "react";
 import LinkButton from "@/components/atoms/LinkButton";
+import { useState } from "react";
 
 const enriqueta = Enriqueta({ weight: "700" });
 
 interface MobileNavbarProps {
-	className?: string; // Define the className prop as optional
+	className?: string;
 }
 
 export default function MobileNavbar({ className }: MobileNavbarProps) {
 	const [isOpen, setIsOpen] = useState(false);
-	const [isVisible, setIsVisible] = useState(false);
 
 	const toggleMenu = () => {
 		setIsOpen(!isOpen);
 	};
-
-	useEffect(() => {
-		if (isOpen) {
-			setIsVisible(true);
-		} else {
-			const timer = setTimeout(() => {
-				setIsVisible(false);
-			}, 300);
-			return () => clearTimeout(timer);
-		}
-	}, [isOpen]);
 
 	return (
 		<div
@@ -63,23 +50,35 @@ export default function MobileNavbar({ className }: MobileNavbarProps) {
 					<span className="block w-6 h-0.5 bg-gray-600"></span>
 				</button>
 			</div>
-			{isVisible && (
-				<div
-					className={`absolute top-21 bg-white shadow-lg rounded-lg mt-2 text-gray-600 w-full left-0 transition-all delay-150 duration-300 ease-in-out transform ${
-						isOpen
-							? "scale-y-100 translate-y-0 opacity-100"
-							: "scale-y-0 translate-y-[-20px] opacity-0 pointer-events-none"
-					}`}
-				>
-					<ul className="flex flex-col p-2">
-						<LinkButton text="HOME" href="/" className="p-2" />
-						<LinkButton text="ABOUT" href="/about" className="p-2" />
-						<LinkButton text="CONTACT" href="/contact" className="p-2" />
-						<LinkButton text="SERVICES" href="/services" className="p-2" />
-						<LinkButton text="Free Stuff" href="/free-stuff" className="p-2" />
-					</ul>
-				</div>
-			)}
+			<div
+				className={`absolute top-21 bg-white shadow-lg rounded-lg mt-2 text-gray-600 w-full left-0 ${
+					isOpen ? "opacity-100 z-20" : "opacity-0 pointer-events-none hidden"
+				}`}
+			>
+				<ul className="flex flex-col items-center p-2">
+					<LinkButton text="HOME" href="/" className="p-2 text-center w-full" />
+					<LinkButton
+						text="ABOUT"
+						href="/about"
+						className="p-2 text-center w-full"
+					/>
+					<LinkButton
+						text="CONTACT"
+						href="/contact"
+						className="p-2 text-center w-full"
+					/>
+					<LinkButton
+						text="SERVICES"
+						href="/services"
+						className="p-2 text-center w-full"
+					/>
+					<LinkButton
+						text="Free Stuff"
+						href="/free-stuff"
+						className="p-2 text-center w-full"
+					/>
+				</ul>
+			</div>
 		</div>
 	);
 }
