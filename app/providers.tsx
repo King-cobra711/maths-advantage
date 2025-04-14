b0cdb318d13dfc0c93c2c1daac2007048d2d4459
Matthew Hansen <72847525+King-cobra711@users.noreply.github.com>
2025-04-14 15:09:18 +1000

    add dynamic configuration for providers. Allows for it to work in dev or prod

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { AuthProvider } from "react-oidc-context";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+const baseUrl = isDevelopment ? "http://localhost:3000" : "http://13.55.185.86";
+
 const cognitoAuthConfig = {
 	authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Jn6Ruf7G7",
 	client_id: "3ogfcls91pc1npf6rgipmk2f7b",
-	redirect_uri: "http://localhost:3000/admin",
-	post_logout_redirect_uri: "http://localhost:3000/admin",
+	redirect_uri: `${baseUrl}/admin`,
+	post_logout_redirect_uri: `${baseUrl}/admin`,
 	response_type: "code",
 	scope: "email openid phone",
 };
