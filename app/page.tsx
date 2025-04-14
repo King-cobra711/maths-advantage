98e29ec72334aeab7e181021d12459fb56a1ab00
Matthew Hansen <72847525+King-cobra711@users.noreply.github.com>
2025-04-14 15:20:48 +1000

    update the deployment script to re-authenticate before pulling (EC2)

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<div className="max-w-[800px] mx-auto">
 			<h1 className="text-4xl py-6 text-teal-600 mx-auto text-center">
-				Welcome to Maths Advantage Hi =)
+				Welcome to Maths Advantage
 			</h1>
 			<p className="text-lg mx-auto text-center pb-8">
 				Your one stop shop for all your maths needs!
